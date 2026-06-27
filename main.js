57f65376b95291733780fd43ca95c71a42df621c
Gibo Blayze <giboblayze0@gmail.com>
2026-06-27 22:11:32 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1,3 +1,15 @@
+const searchBox = document.getElementById("searchBox");
+
+searchBox.addEventListener("keyup", function () {
+    const filter = this.value.toLowerCase();
+    const links = document.querySelectorAll(".section a");
+
+    links.forEach(link => {
+        link.style.display = link.textContent.toLowerCase().includes(filter)
+            ? "block"
+            : "none";
+    });
+});
 function loadMore(sectionId, button){
     const hidden = document.querySelectorAll("#"+sectionId+" .hidden");
 
