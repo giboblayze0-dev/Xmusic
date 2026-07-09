c1ffa5098f309df8c95bafb90195fe235a3a84a8
Gibo Blayze <giboblayze0@gmail.com>
2026-07-09 21:31:40 +0200

    Update main.js

--- main.js
+++ main.js
@@ -15,40 +15,42 @@ fetch("music.json")
 .then(data => {
 
   data.forEach(song => {
-    songs[song.section].push(song);
+    if (songs[song.section]) {
+      songs[song.section].push(song);
+    }
   });
 
-  showSection("new");
-  showSection("trending");
-  showSection("album");
+  show("new");
+  show("trending");
+  show("album");
 
 });
 
 
-function showSection(section){
+function show(section){
 
   let html = "";
 
   songs[section].slice(0, limit[section]).forEach(song => {
 
     html += `
-    <div class="song-card">
-      <a href="${song.link}">
-        <img src="${song.image}" alt="${song.title}">
-        <h3>${song.title}</h3>
-        <p>${song.artist}</p>
-      </a>
-    </div>`;
+      <div class="song-card">
+        <a href="${song.link}">
+          <img src="${song.image}" alt="music">
+        </a>
+      </div>
+    `;
 
   });
 
   document.getElementById(section).innerHTML = html;
+
 }
 
 
 function loadMore(section){
 
   limit[section] += 6;
-  showSection(section);
+  show(section);
 
 }
