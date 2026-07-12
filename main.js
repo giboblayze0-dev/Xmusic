2ef076a2c5a8d518e612930f4765d46855601626
Gibo Blayze <giboblayze0@gmail.com>
2026-07-12 08:29:48 +0200

    Remove limit object and loadMore function
    Removed limit object and loadMore function.

--- main.js
+++ main.js
@@ -1,13 +1,3 @@
-
-const limit = {
-  new: 6,
-  trending: 6,
-  album: 6
-};
-function loadMore(section) {
-  limit[section] += 6;
-  show(section);
-}
 const songs = {
   new: [],
   trending: [],
